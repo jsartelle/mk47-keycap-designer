dcc0c8a586853d5828a53eb2edfee8313fe4e007
James Sartelle <james.sartelle@extra.app>
2023-08-25 23:20:29 -0500

    Store layout in settings instead

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import Key from '$lib/components/Key.svelte'
-	import layout from '$lib/data/layout.json'
+	import { layout } from '$lib/stores/settings'
 </script>
 
 <section class="keyboard">
-	{#each layout as key, index}
+	{#each $layout as key, index}
 		<Key width={index === 41 ? 2 : 1}>
 			<span>{key[0]}</span>
 			<span slot="layer1">{key[1]}</span>
@@ -20,4 +20,3 @@
 		font-size: 75%;
 	}
 </style>
-s
