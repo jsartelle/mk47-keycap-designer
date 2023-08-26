53dd1d3487640df86458e7f9956746230d23e916
James Sartelle <james.sartelle@extra.app>
2023-08-25 23:40:00 -0500

    Move settings to separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Keyboard from '$lib/components/Keyboard.svelte'
-	import { defaultSettings } from '$lib/stores/store'
+	import Settings from '$lib/components/Settings.svelte'
 </script>
 
 <main>
@@ -11,41 +11,6 @@
 	<section class="settings">
 		<h1>Default Settings</h1>
 
-		<fieldset class="grid">
-			<legend>Fonts</legend>
-			<label>
-				<input type="text" bind:value={$defaultSettings.fontBase} placeholder="system-ui" />
-				<span>Main</span>
-			</label>
-			<label>
-				<input type="text" bind:value={$defaultSettings.fontLayer1} placeholder="system-ui" />
-				<span>Layer 1</span>
-			</label>
-			<label>
-				<input type="text" bind:value={$defaultSettings.fontLayer2} placeholder="system-ui" />
-				<span>Layer 2</span>
-			</label>
-		</fieldset>
-
-		<fieldset class="grid">
-			<legend>Colors</legend>
-			<label>
-				<input type="color" bind:value={$defaultSettings.background} />
-				<span>Background</span>
-			</label>
-			<label>
-				<input type="color" bind:value={$defaultSettings.colorBase} />
-				<span>Base Text</span>
-			</label>
-			<label>
-				<input type="color" bind:value={$defaultSettings.colorLayer1} />
-				<span>Layer 1 Text</span>
-			</label>
-			<label>
-				<input type="color" bind:value={$defaultSettings.colorLayer2} />
-				<span>Layer 2 Text</span>
-			</label>
-		</fieldset>
-		<!-- TODO text alignment, casing, make smaller and make this a component that can be applied to per-key overrides with a popover -->
+		<Settings />
 	</section>
 </main>
