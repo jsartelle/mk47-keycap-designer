0d26d6d2a624f1675a4a9170a12ece83181f47ed
James Sartelle <james.sartelle@extra.app>
2023-09-01 00:36:32 -0500

    Indicate scroll on key settings with gradient

--- src/lib/components/Keyboard.svelte
+++ src/lib/components/Keyboard.svelte
@@ -2,23 +2,38 @@
 	import Key from '$lib/components/Key.svelte'
 	import KeySettings from '$lib/components/KeySettings.svelte'
 	import { keyboardBackground, perKeySettings } from '$lib/stores/store'
-	import type { ComponentEvents } from 'svelte'
+	import { tick, type ComponentEvents } from 'svelte'
 
 	type KeyEvents = ComponentEvents<Key>
 
 	let keySettingsPopup: HTMLDialogElement
+	let keySettingsContainer: HTMLDivElement
 	let keySettingsIndex: number
 	let keyTop: string
 	let keyLeft: string
 	let keyWidth: string
 
-	function openKeySettings(event: KeyEvents['click']) {
+	let keySettingsOverflowTop: boolean
+	let keySettingsOverflowBottom: boolean
+
+	async function openKeySettings(event: KeyEvents['click']) {
 		keySettingsIndex = event.detail.index
 		keyTop = event.detail.rect.bottom + 'px'
 		keyLeft = event.detail.rect.left + 'px'
 		keyWidth = event.detail.rect.width + 'px'
 
 		keySettingsPopup.showModal()
+		await tick()
+		keySettingsContainer.scrollTo(0, 0)
+		keySettingsScroll()
+	}
+
+	function keySettingsScroll() {
+		keySettingsOverflowTop = keySettingsPopup ? keySettingsContainer.scrollTop > 0 : false
+		keySettingsOverflowBottom = keySettingsPopup
+			? keySettingsContainer.scrollTop + keySettingsContainer.clientHeight <
+			  keySettingsContainer.scrollHeight
+			: false
 	}
 
 	function closeKeySettings() {
@@ -35,7 +50,11 @@
 <dialog bind:this={keySettingsPopup}>
 	<button class="close-dialog" on:click={closeKeySettings} aria-label="Close Key Settings" />
 	<div
+		bind:this={keySettingsContainer}
 		class="key-settings"
+		on:scroll={keySettingsScroll}
+		class:overflow-top={keySettingsOverflowTop}
+		class:overflow-bottom={keySettingsOverflowBottom}
 		style:--key-top={keyTop}
 		style:--key-left={keyLeft}
 		style:--key-width={keyWidth}
@@ -91,7 +110,32 @@
 		max-height: calc(100vh - var(--key-top) - var(--spacing));
 		border-radius: var(--border-radius);
 		overflow: hidden scroll;
-		resize: horizontal;
 		container-type: inline-size;
+	}
+
+	.key-settings.overflow-top::before,
+	.key-settings.overflow-bottom::after {
+		--gradient-position: center 0;
+		content: '';
+		display: block;
+		position: sticky;
+		top: 0;
+		left: 0;
+		transform: translate(calc(var(--spacing) * -1), calc(var(--spacing) * -1));
+		height: var(--spacing);
+		width: inherit;
+		background: radial-gradient(
+			75% 100% at var(--gradient-position),
+			var(--form-element-border-color),
+			transparent
+		);
+		pointer-events: none;
+	}
+
+	.key-settings.overflow-bottom::after {
+		--gradient-position: center 100%;
+		top: auto;
+		bottom: 0;
+		transform: translate(calc(var(--spacing) * -1), var(--spacing));
 	}
 </style>
